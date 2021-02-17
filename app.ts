571ab2a2e348709ededa8640cbc40030b8d72edd
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-17 22:49:12 +0530

    feat: intro to union types

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("Hello world");
+function combine(n1: number | string, n2: number | string) {
+  let result;
+  if (typeof n1 === "number" && typeof n2 === "number") {
+    result = n1 + n2;
+  } else {
+    result = n1.toString() + n2.toString();
+  }
+
+  return result;
+}
+
+const combineAges = combine(30, 46);
+console.log(combineAges);
+
+const combineNames = combine("x", "y");
+console.log(combineNames);
