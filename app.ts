a9e09343d937da8f46876407a5f425224cbc6ca0
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-18 00:19:16 +0530

    feat: function and callback types

--- app.ts
+++ app.ts
@@ -13,4 +13,14 @@ combineValues = add;
 // combineValues = 5;
 // combineValues = printResult;
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void): void {
+  const result = n1 + n2;
+  cb(result);
+}
+
 console.log(combineValues(10, 3));
+
+addAndHandle(10, 2, (result) => {
+  console.log(result);
+  return result; // this will be ignored as function type is void and will not raise an error
+});
