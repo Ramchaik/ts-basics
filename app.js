b537c6e2fd7bfe30e04245ce655320149df90169
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-18 23:50:29 +0530

    feat: wrap up types

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Vaibhav";
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, statusCode: code };
+}
+function infiniteLoop() {
+    while (true) { }
+}
