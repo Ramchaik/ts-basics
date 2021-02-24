4c23f0a1e65612e56e94cf235d3d4c9914e1ac3d
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-24 10:20:09 +0530

    feat(next-gen): :sparkles: default function parameters

--- src/next-gen/app.ts
+++ src/next-gen/app.ts
@@ -1,6 +1,7 @@
-const add1 = (a: number, b: number) => a + b;
+const add1 = (a: number, b: number = 1) => a + b;
 const printOutput = (output: string | number) => console.log(output);
 const printOutput1: (a: number | string) => void = (output) =>
   console.log(output);
 
 printOutput(add1(1, 2));
+printOutput(add1(2));
