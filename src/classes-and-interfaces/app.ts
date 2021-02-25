3fd1d8a2e0ce03aaf61f3e78bbbc544130583329
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-25 22:55:51 +0530

    feat: `this` reference issue

--- src/classes-and-interfaces/app.ts
+++ src/classes-and-interfaces/app.ts
@@ -4,7 +4,16 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  describe() {
+    console.log("Department: " + this.name);
+  }
 }
 
 const tech = new Department("Tech");
 console.log(tech);
+
+tech.describe();
+
+const techCopy = { describe: tech.describe };
+techCopy.describe();
