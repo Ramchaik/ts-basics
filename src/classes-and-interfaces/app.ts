989055113c20a3a750e540f6a73619a9fdafcd2e
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-25 23:14:25 +0530

    feat: default public access modifier

--- src/classes-and-interfaces/app.ts
+++ src/classes-and-interfaces/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
@@ -8,6 +9,15 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const tech = new Department("Tech");
@@ -15,5 +25,12 @@ console.log(tech);
 
 tech.describe();
 
-const techCopy = { name: "DUMMY", describe: tech.describe };
-techCopy.describe();
+tech.addEmployee("raju");
+tech.addEmployee("chacha");
+
+tech.employees[2] = "mukesh";
+
+tech.printEmployeeInfo();
+
+// const techCopy = { name: "DUMMY", describe: tech.describe };
+// techCopy.describe();
