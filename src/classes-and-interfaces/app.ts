643a093ae7471ef7ab344af9ef1a90fc849d1fb7
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-28 22:23:12 +0530

    feat: :sparkles: getters and setters

--- src/classes-and-interfaces/app.ts
+++ src/classes-and-interfaces/app.ts
@@ -30,12 +30,31 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReported: string;
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReported = reports[0]!;
+  }
+
+  get mostRecentReport() {
+    if (this.lastReported) {
+      return this.lastReported;
+    }
+
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value");
+    }
+    this.addReport(value);
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReported = text;
   }
 
   addEmployee(employee: string) {
@@ -62,11 +81,16 @@ it.addEmployee("chacha");
 it.printEmployeeInfo();
 
 const accounting = new AccountingDepartment("d2", []);
+
 accounting.addEmployee("x");
 accounting.addEmployee("Vaibhav");
 accounting.printEmployeeInfo();
 
 accounting.addReport("Something went wrong");
+// * Setter
+accounting.mostRecentReport = "Year end Report";
+// * Getter
+console.log(accounting.mostRecentReport);
 
 console.log(accounting);
 
