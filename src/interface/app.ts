1c1d517ab2cf95deb5f26070a403d5b87c261310
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-01 22:48:54 +0530

    feat(interface): interface as function types

--- src/interface/app.ts
+++ src/interface/app.ts
@@ -21,3 +21,14 @@ p1 = new PersonX("John", 36);
 // p1.name = "y"; // error as property is readonly
 
 p1.greet("Waba laba dub dub");
+
+type AddFn = (a: number, b: number) => number;
+interface IAddFn {
+  (a: number, b: number) => number;
+}
+
+let addFn: AddFn;
+addFn = (n1: number, n2: number) => n1 + n2
+
+let addFn2: IAddFn;
+addFn2 = (n1: number, n2: number) => n1 + n2 
