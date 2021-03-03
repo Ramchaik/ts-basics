88d9d0b50ddb29c185a0a7599d5ec22e3bb7339f
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-03 09:50:08 +0530

    feat: optional parameters and properties

--- src/interface/app.ts
+++ src/interface/app.ts
@@ -1,34 +1,40 @@
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
-interface X {
-  age: number;
-}
-interface Greetable extends Named, X {
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class PersonX implements Greetable {
-  constructor(public name: string, public age: number) {}
+  constructor(public name?: string) {}
 
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if (!this.name) {
+      console.log("hi!");
+    } else {
+      console.log(phrase + " " + this.name);
+    }
   }
 }
 
 let p1: Greetable;
-p1 = new PersonX("John", 36);
+p1 = new PersonX("John");
+
+const p2 = new PersonX();
+p2.greet("whatsupp!")
+
 // p1.name = "y"; // error as property is readonly
 
 p1.greet("Waba laba dub dub");
 
 type AddFn = (a: number, b: number) => number;
 interface IAddFn {
-  (a: number, b: number) => number;
+  (a: number, b: number): number;
 }
 
 let addFn: AddFn;
-addFn = (n1: number, n2: number) => n1 + n2
+addFn = (n1: number, n2: number) => n1 + n2;
 
 let addFn2: IAddFn;
-addFn2 = (n1: number, n2: number) => n1 + n2 
+addFn2 = (n1: number, n2: number) => n1 + n2;
