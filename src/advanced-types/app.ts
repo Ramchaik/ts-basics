4cfe4d074bbd78b2e176c6f8585629c0c2326d39
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-14 00:26:02 +0530

    chore: un-comment code

--- src/advanced-types/app.ts
+++ src/advanced-types/app.ts
@@ -55,125 +55,125 @@ function addVals(a: Combinable, b: Combinable) {
 const result = addVals('vaibhav  ', 'singh');
 result.split(' ');
 
-// // ** type guard with `in`
-// type UnknownEmployee = Employee | Admin;
+// ** type guard with `in`
+type UnknownEmployee = Employee | Admin;
 
-// function printEmployeeInfo(emp: UnknownEmployee) {
-//   console.log("Name: " + emp.name);
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
 
-//   if ("privileges" in emp) {
-//     console.log("Privileges: " + emp.privileges);
-//   }
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
 
-//   if ("startDate" in emp) {
-//     console.log("Start Date: " + emp.startDate);
-//   }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInfo({ name: "Raju", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving a car...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo...", amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  // * Type guard
+  // if ('loadCargo' in vehicle) {
+  //   vehicle.loadCargo(100);
+  // }
+  // * Type guard with `instanceof`, Elegant solution for classes
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(100);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+// * Discriminated Unions (pattern)
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+// * `in` is the only solution, you cannot use `instanceof` with interfaces as the object is not mapped to a type
+function moveAnimal(animal: Animal) {
+  let speed;
+
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+
+  console.log("Animal is moving at " + speed);
+}
+
+moveAnimal({ type: "horse", runningSpeed: 10 });
+
+// * Type Casting: forcing TS to infer the value's type as the passed type
+
+// * Problem: TS can't infer the type
+// const userInputElement = document.getElementById('user-input')!;
+// userInputElement.value = 'x'; // will throw an error as TS can't infer the type of the element is input
+
+// * Type Casting using `< >`
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+// userInputElement.value = "x";
+
+// * Type Casting using `as`
+// const userInputElement2 = document.getElementById(
+//   "user-input"
+// )! as HTMLInputElement;
+// userInputElement2.value = "x";
+
+// * Type casting, where we can have null values
+// const userInputElement3 = document.getElementById("user-input");
+// if (userInputElement3) {
+//   (userInputElement3 as HTMLInputElement).value = "x";
 // }
 
-// printEmployeeInfo({ name: "Raju", startDate: new Date() });
+// * Index Properties
+interface ErrorContainer {
+  [prop: string]: string; // * don't know the exact property name and also don't know the property count
+}
 
-// class Car {
-//   drive() {
-//     console.log("Driving a car...");
-//   }
-// }
-
-// class Truck {
-//   drive() {
-//     console.log("Driving a truck...");
-//   }
-
-//   loadCargo(amount: number) {
-//     console.log("Loading cargo...", amount);
-//   }
-// }
-
-// type Vehicle = Car | Truck;
-// const v1 = new Car();
-// const v2 = new Truck();
-
-// function useVehicle(vehicle: Vehicle) {
-//   vehicle.drive();
-
-//   // * Type guard
-//   // if ('loadCargo' in vehicle) {
-//   //   vehicle.loadCargo(100);
-//   // }
-//   // * Type guard with `instanceof`, Elegant solution for classes
-//   if (vehicle instanceof Truck) {
-//     vehicle.loadCargo(100);
-//   }
-// }
-
-// useVehicle(v1);
-// useVehicle(v2);
-
-// // * Discriminated Unions (pattern)
-
-// interface Bird {
-//   type: "bird";
-//   flyingSpeed: number;
-// }
-
-// interface Horse {
-//   type: "horse";
-//   runningSpeed: number;
-// }
-
-// type Animal = Bird | Horse;
-
-// // * `in` is the only solution, you cannot use `instanceof` with interfaces as the object is not mapped to a type
-// function moveAnimal(animal: Animal) {
-//   let speed;
-
-//   switch (animal.type) {
-//     case "bird":
-//       speed = animal.flyingSpeed;
-//       break;
-
-//     case "horse":
-//       speed = animal.runningSpeed;
-//       break;
-//   }
-
-//   console.log("Animal is moving at " + speed);
-// }
-
-// moveAnimal({ type: "horse", runningSpeed: 10 });
-
-// // * Type Casting: forcing TS to infer the value's type as the passed type
-
-// // * Problem: TS can't infer the type
-// // const userInputElement = document.getElementById('user-input')!;
-// // userInputElement.value = 'x'; // will throw an error as TS can't infer the type of the element is input
-
-// // * Type Casting using `< >`
-// // const userInputElement = <HTMLInputElement>(
-// //   document.getElementById("user-input")!
-// // );
-// // userInputElement.value = "x";
-
-// // * Type Casting using `as`
-// // const userInputElement2 = document.getElementById(
-// //   "user-input"
-// // )! as HTMLInputElement;
-// // userInputElement2.value = "x";
-
-// // * Type casting, where we can have null values
-// // const userInputElement3 = document.getElementById("user-input");
-// // if (userInputElement3) {
-// //   (userInputElement3 as HTMLInputElement).value = "x";
-// // }
-
-// // * Index Properties
-// interface ErrorContainer {
-//   [prop: string]: string; // * don't know the exact property name and also don't know the property count
-// }
-
-// const errorBag: ErrorContainer = {
-//   email: "Not a valid email1",
-//   username: "Must start with capital character!",
-// };
+const errorBag: ErrorContainer = {
+  email: "Not a valid email1",
+  username: "Must start with capital character!",
+};
 
 // * Optional Chaining
 const fetchedUserData = {
