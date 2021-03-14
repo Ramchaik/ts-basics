e7537aba75693edc0622d8c5e249841e1c6868c1
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-14 12:46:51 +0530

    feat: custom function generics

--- src/generics/app.ts
+++ src/generics/app.ts
@@ -1,12 +1,21 @@
 // * Generics
-const names: Array<string> = []; // * generic way
+// const names: Array<string> = []; // * generic way
 // OR
 //const names: string[] =[]
 
 // generics set to number
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(10), 2000);
-});
-promise.then((data) => {
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(10), 2000);
+// });
+// promise.then((data) => {
   // data.split(' )
-});
+// });
+
+// * Custom Generics 
+function merge<T, U> (objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObject = merge({name: 'vaibhav', hobbies: ['A']}, {age: 24});
+console.log(mergeObject);
+
