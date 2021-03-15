92f7141fa3e4869435d7bf1d0a3050fdc97004cd
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-15 08:57:46 +0530

    feat: another generic function

--- src/generics/app.ts
+++ src/generics/app.ts
@@ -8,15 +8,32 @@
 //   setTimeout(() => resolve(10), 2000);
 // });
 // promise.then((data) => {
-  // data.split(' )
+// data.split(' )
 // });
 
-// * Custom Generics with constraints 
-function merge<T extends object, U extends object> (objA: T, objB: U) {
+// * Custom Generics with constraints
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 // const mergeObject = merge({name: 'vaibhav', hobbies: ['A']}, 24); // ! throws error as arguments are now constraint
-const mergeObject = merge({name: 'vaibhav', hobbies: ['A']}, {age: 24});
+const mergeObject = merge({ name: "vaibhav", hobbies: ["A"] }, { age: 24 });
 console.log(mergeObject);
 
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = "Got no value";
+
+  if (element.length === 1) {
+    description = "Got 1 value.";
+  } else if (element.length > 1) {
+    description = "Got " + element.length + " value.";
+  }
+
+  return [element, description];
+}
+
+console.log(countAndDescribe(['a', 'b']));
