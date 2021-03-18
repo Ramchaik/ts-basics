9a2dcaaec37620626aa5032357e6e8f4d91b7a44
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-18 10:50:28 +0530

    feat(decorators): adding multiple decorators

--- src/decorators/app.ts
+++ src/decorators/app.ts
@@ -6,6 +6,7 @@ function Logger(constructor: Function) {
 
 function WithTemplate(template: string, hookId: string) {
   return function(constructor: any) {
+    console.log('Render template');
     const p = new constructor();
     const hookEl = document.getElementById(hookId);
     if (hookEl) {
@@ -15,14 +16,16 @@ function WithTemplate(template: string, hookId: string) {
   }
 }
 
+// * Decorator Factory's order of execution is top-down
+// * Actual Decorator's order of execution is bottom-down
 @WithTemplate('<h1> Hello world </h1>', 'app')
+@LoggerFactory("LOGGING - PERSON")
 class Person {
   name = 'vaibhav';
   constructor() {
     console.log('Creating a new Object');
   }
 }
-
 
 console.log(' ---------- ');
 
