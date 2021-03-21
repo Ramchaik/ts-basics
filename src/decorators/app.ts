2c000d5cd4168947a735db6aae6a07f06184b573
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-21 15:53:51 +0530

    feat(decorator): autobind decorator using Method decorator

--- src/decorators/app.ts
+++ src/decorators/app.ts
@@ -117,3 +117,29 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function Autobind(_:any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescriptor;
+}
+class Printer {
+  message = 'this works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+    
+  }
+}
+
+const printer = new Printer();
+const button = document.querySelector('button')!;
+button.addEventListener('click', printer.showMessage)
